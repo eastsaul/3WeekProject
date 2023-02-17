f1902296789e73c717cabc445247450cfe7bb974
ymg <freedman0829@naver.com>
2023-02-17 17:39:03 +0900

    Dev 0217 전투 화면

--- WinterTower/Assets/Script/EnemyHpGuage.cs
+++ WinterTower/Assets/Script/EnemyHpGuage.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using static Unity.VisualScripting.Icons;
 
-public class GuageSystem : MonoBehaviour
+public class EnemyHpGuage : MonoBehaviour
 {
     private Image frontGuage;
 
-    public int guageNow = default;
-    public int maxGuage = default;
-    public float guageControll = default;
+    public int eGuage = default;
+    public int eGuageMax = default;
+    public float eGuageControll = default;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +23,10 @@ public class GuageSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        guageNow = EnemyInfo.monsterHp;
-        maxGuage = EnemyInfo.monsterMaxHp;
+        eGuage = EnemyInfo.monsterHp;
+        eGuageMax = EnemyInfo.monsterMaxHp;
 
-        guageControll = guageNow / (float)maxGuage;
-        frontGuage.fillAmount= guageControll;
+        eGuageControll = eGuage / (float)eGuageMax;
+        frontGuage.fillAmount= eGuageControll;
     }
 }
